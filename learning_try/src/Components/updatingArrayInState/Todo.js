f940de0fb60598638ce3c117862abfee36640723
Bishal487 <bishal.kunwar487@gmail.com>
2023-09-21 15:49:05 -0400

    some practices with the statem management and some functions to buttons without the mutations.

--- learning_try/src/Components/updatingArrayInState/Todo.js
+++ learning_try/src/Components/updatingArrayInState/Todo.js
@@ -13,6 +13,29 @@ export default function Todo(){
     const[name, setName] = useState('');
     const[artists, setArtists] = useState(initialArtists);
 
+    // Reverse the array method.
+    function handleReverse(){
+        const nextList = [...artists];
+        nextList.reverse();
+        setArtists(nextList);
+    };
+
+    function handleAscSort(){
+        const nextList = [...artists];
+        nextList.sort((a, b) => a.name.localeCompare(b.name));
+        //nextList.sort((a,b)=> a.name>b.name ? 1 : a.name===b.name ? 0 : -1);
+        // console.log(nextList);
+        setArtists(nextList);
+    }
+
+    function handleDescSort(){
+        const nextList = [...artists];
+        nextList.sort((a,b)=> b.name.localeCompare(a.name));
+        // nextList.sort((a,b)=> a.name< b.name ? 1 : a.name===b.name ? 0 : -1 );
+        setArtists(nextList);
+    }
+
+
     return(
         <>
             <h1>Coder's Lists</h1>
@@ -27,9 +50,22 @@ export default function Todo(){
                 Add
             </button>
 
+            <button onClick={handleReverse}>
+                Reverse Artist 
+            </button>
+
+            <button onClick={handleAscSort}>
+                Asc Sort
+            </button> 
+
+            
+            <button onClick={handleDescSort}>
+                Desc Sort
+            </button>
+
             <ul>
                 {/* {artists.map(artist=>(<li key={artist.id}>{artist.name}</li>))} */}
-                {artists.map(artist=>(<li>{artist.name}{''} <button onClick={()=>{setArtists(artists.filter(a=> a.id !== artist.id))}}>Delete</button></li>))}
+                {artists.map(artist=>(<li key={artist.id}>{artist.name}{''} <button onClick={()=>{setArtists(artists.filter(a=> a.id !== artist.id))}}>Delete</button></li>))}
                 
             </ul>
 
